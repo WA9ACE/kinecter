abc47b57a88ea705d969d2416025ea4ed0bdeedc
Caleb Albritton <ithinkincode@gmail.com>
2013-07-11 14:23:55 -0500

    added argument checks

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,35 @@ using namespace std;
 Handle<Value> setViewAngle(const Arguments& args) {
 	HandleScope scope;
 
+	if(args.Length() != 1) {
+		return ThrowException(
+			Exception::TypeError(
+				String::New("Wrong number of arguments. setViewAngle(n) requires a single int from -27 to 27")
+			)
+		);
+	}
+
+	Local<Integer> integer = args[0]->ToInteger();
+	int angle = integer->Value();
+
+	if(angle > 27) {
+		return ThrowException(
+			Exception::TypeError(
+				String::New("Angle greater than 27. setViewAngle(n) requires a single int from -27 to 27")
+			)
+		);
+	} else if(angle < -27) {
+		return ThrowException(
+			Exception::TypeError(
+				String::New("Angle less than -27. setViewAngle(n) requires a single int from -27 to 27")
+			)
+		);
+	}
+
 	KinectSensor *pSensor = new KinectSensor();
 	Kinecter *pKinecter = new Kinecter();
 
-	pSensor->setViewAngle(args[0]->NumberValue());
+	pSensor->setViewAngle(angle);
 
 	return scope.Close(Undefined());
 }
